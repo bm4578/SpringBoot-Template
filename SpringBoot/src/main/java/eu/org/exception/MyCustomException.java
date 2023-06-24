c6927b87e7cc317d77b4e33e915bf66e692a8281
codetype <m15319631269@163.com>
2023-06-24 21:40:45 +0800

    使用说明

--- SpringBoot/src/main/java/eu/org/exception/MyCustomException.java
+++ SpringBoot/src/main/java/eu/org/exception/MyCustomException.java
@@ -3,7 +3,9 @@ package eu.org.exception;
 /**
  * @author 白也
  * @date 2023/6/24 8:34 下午
- * @title
+ * @title 使用：
+ *   //验证密码
+ *  if (! user.getPassword().equals(loginForm.getPassword())) throw new MyCustomException("密码错误!!!");
  */
 
 public class MyCustomException extends RuntimeException {
